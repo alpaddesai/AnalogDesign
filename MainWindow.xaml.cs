cce1679c9f7e2be42a13ec46eb9be08bcd740b90
unknown <alpa.gupta@gmail.com>
2019-06-08 21:46:56 -0400

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnalogDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void SmallSignalModel_Click(object sender, RoutedEventArgs e)
+        {
+            SmallSignalModel smallSignalObject = new SmallSignalModel();
+            smallSignalObject.Show();
+        }
+
+        private void DifferentialAmplifiers_Click(object sender, RoutedEventArgs e)
+        {
+            DifferentialAmplifiers diffamplifersObject = new DifferentialAmplifiers();
+            diffamplifersObject.Show();
+        }
+
+        private void CMOS_Amplifiers_Click(object sender, RoutedEventArgs e)
+        {
+            CMOSAmplifiers CMOSAmplifiersobject = new CMOSAmplifiers();
+            CMOSAmplifiersobject.Show();
+        }
+
+        private void Operational_Amplifers_Click(object sender, RoutedEventArgs e)
+        {
+            Operational_Amplifiers Operational_AmplifiersObject = new Operational_Amplifiers();
+            Operational_AmplifiersObject.Show();
+        }
+    }
+}
---
+++ SmallSignalModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogDesign
+{
+    public partial class SmallSignalModel : Form
+    {
+        public SmallSignalModel()
+        {
+            InitializeComponent();
+        }
+
+        private void SmallSignalModel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DigitalTiming_Click(object sender, EventArgs e)
+        {
+            DigitalTimingCharacteristics DigitalTimingCharacteristicsobject = new DigitalTimingCharacteristics();
+                DigitalTimingCharacteristicsobject.Show();
+        }
+    }
+}
